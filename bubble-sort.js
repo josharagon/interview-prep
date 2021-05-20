f0c66af903f116633e228e2e0573f58357c34ee8
Josh Aragon <josharagon99@gmail.com>
2021-05-19 23:19:16 -0600

    Add bubble sort function that returns number of swaps it takes to put array in order

---
+++ bubble-sort.js
@@ -0,0 +1,25 @@
+function countSwaps(a) {
+  let moveCount = 0;
+  let isSorted = false;
+  let lastUnsorted = a.length - 1
+  while(!isSorted){
+      isSorted = true;
+      for(var i = 0; i < lastUnsorted; i++) {
+          if(a[i] > a[i + 1]) {
+              let temp = a[i];
+              a[i] = a[i+1];
+              a[i+1] = temp;
+              i=0;
+              moveCount++
+              isSorted = false;
+              } 
+          }
+          lastUnsorted--
+  }
+  console.log(`Array is sorted in ${moveCount} swaps.
+First Element: ${a[0]}
+Last Element: ${a.pop()}`)
+}
+
+
+countSwaps([4, 5, 2, 1, 10, 3, 6, 7, 8, 9])
